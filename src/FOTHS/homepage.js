1a8ee2ce04e7aa46b3aed57ae4595501cda11fa0
Keerthana <keerthanas5112@gmail.com>
2025-08-27 12:58:33 +0530

    made somechanges and added code in js

--- src/FOTHS/homepage.js
+++ src/FOTHS/homepage.js
@@ -1,1 +1,10 @@
-<form action="javascript:void(0);" onsubmit="alert('Subscribed!');"></form>
+// Select the form
+const newsletterForm = document.getElementById("newsletter-form");
+
+// Add event listener for form submit
+newsletterForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevents page reload
+  alert("Subscribed!");
+});
+    newsletterForm.reset(); // Resets the form fields
+    
